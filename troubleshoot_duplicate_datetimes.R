96be4409d2eecbc847325530f6550709963f57fb
Annie <annie.kellner@colostate.edu>
2018-07-13 11:14:26 -0600

    Decision to repeat all steps for Land Designation using R only

--- troubleshoot_duplicate_datetimes.R
+++ troubleshoot_duplicate_datetimes.R
@@ -24,10 +24,45 @@ head(land[land$tt2==T,])
 tail(land[land$tt2==T,])
 table(land[land$tt2==T,"animal"]) #duplicates by animal 
 
+test <- land[land$tt2==T,]
 
-
+## earlier land.csv file does work
+##########################################################
 
 land2 <- read.csv("C:/Users/akell/Desktop/Spring_2018/Research/GIS/Chapter1/Land.csv")
 land2$datetime2 <- ymd_hms(paste(land2$year, land2$month, land2$day, land2$hour, land2$minute, land2$second, sep = "-"), tz="US/Alaska") #use lubridate fxn to format datetime, add column to land2 dataframe
-
 traj.land2 <- as.ltraj(xy=land2[,c("X","Y")], date=land2$datetime2, id=as.character(land2$animal))
+
+###########################################
+orig <- read.csv("usgs_pbear_gps_ccde16_v20170131.csv")
+
+library(dplyr)
+
+orig$dup <- duplicated(orig, incomparables = FALSE) #duplicates in df
+head(orig$dup)
+table(orig$dup)
+
+orig$test<- paste(orig$animal, orig$year, orig$month, orig$day, orig$hour, orig$minute, orig$second)
+head(orig$test) #column with just animal and date/time
+
+orig$pastedup <- duplicated(orig$test, incomparables = FALSE) #duplicates in test column 
+head(orig$pastedup)
+table(orig$pastedup)
+
+## DUPLICATES ARE NOT IN ORIGINAL DATA
+################################################
+
+ows <- read.csv("ows_complete_v1.csv")
+ows$dup <- duplicated(ows, incomparables = FALSE) #duplicates in df
+head(ows$dup)
+table(ows$dup)
+head(ows) #no duplicates but realized need to delete FID column
+
+ows2 <- select(ows, animal:datetime)
+head(ows2)
+
+ows2$dup <- duplicated(ows2, incomparables = FALSE) #duplicates in df
+head(ows2$dup)
+table(ows2$dup)
+
+## Decision to repeat Land Designation in R ##
