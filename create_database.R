b7d69ce3b74c235dcb4a2a7dcee2e6a576249fd7
Annie <annie.kellner@colostate.edu>
2018-08-19 15:20:39 -0700

    add land and swim data to mydata

--- create_database.R
+++ create_database.R
@@ -4,47 +4,36 @@ library(dplyr)
 library(adehabitatLT)
 library(lubridate)
 
+## Prep database from TA
 all <- read.csv("usgs_pbear_gps_ccde16_v20170131.csv")
 all <- dplyr::select(all, animal:date) #another loaded package probably has a 'select' fxn, so specify dplyr
+all$datetime <- as.character(ymd_hms(paste(all$year, all$month, all$day, all$hour, all$minute, all$second, sep = "-"), tz="US/Alaska"))
+all <- na.omit(all) # delete datetimes that failed to parse
+
+### land ###
 land <- read.csv("Land.csv")
 land<-dplyr::select(land, animal:date, datetime)
-
-#make sure dataframes have identical columns
-all$datetime <- ymd_hms(paste(all$year, all$month, all$day, all$hour, all$minute, all$second, sep = "-"), tz="US/Alaska")
-all <- na.omit(all) # delete datetimes that failed to parse
-land$distance<- land$distanc
-land$data_origin <- land$dat_rgn
-
-#paste into common column
-land$tt<-paste(land$animal, land$datetime) #land
+land$tt<-paste(land$animal, land$datetime) 
 all$tt<-paste(all$animal, all$datetime)
-
 tt2<-land$tt
-
 all$land<-ifelse(all$tt %in% tt2, 1, 0)
 
-swim <- read.csv("Landing.csv") #swim
-swim <- na.omit(swim) #remove NAs
-swim<-dplyr::select(swim, ID:End.Date) #eliminate unnecessary columns
+#### swim ###
 
-#Format datetime
-swim$Start.Date <- mdy_hms(swim$Start.Date)
-swim$Start.Date <- as.character(swim$Start.Date)
-swim$End.Date <- mdy_hms(swim$End.Date)
-swim$End.Date<- as.character(swim$End.Date)
+swim <- read.csv("swim.csv")
+swim<-dplyr::select(swim, ID:end.time)
+swim <- na.omit(swim)
 
-swim$tt <- paste(swim$ID, swim$Start.Date) 
+swim$tt <- paste(swim$ID, swim$start.datetime) 
 tt3 <- swim$tt
 all$start.swim <- ifelse(all$tt %in% tt3, 1,0)
 
-swim$tt.end <- paste(swim$ID, swim$End.Date) #end swim
-tt4 <- swim$tt.end
-all$end.swim <- ifelse(all$tt %in% tt4,1,0)
-
-write.csv(all, "data080918.csv")
+## save df as .Rdata
+mydata <- 
 
 
-class(all$year)
+
+
 
 ###########################
 ### JUNK #################
