4f8b8e503c9bbfdf86105baa9bdb49992f8e723e
Annie <annie.kellner@colostate.edu>
2018-08-20 13:26:17 -0600

    final

--- create_database.R
+++ create_database.R
@@ -13,6 +13,7 @@ all <- na.omit(all) # delete datetimes that failed to parse
 ### land ###
 land <- read.csv("Land.csv")
 land<-dplyr::select(land, animal:date, datetime)
+land <- na.omit(land)
 land$tt<-paste(land$animal, land$datetime) 
 all$id.datetime<-paste(all$animal, all$datetime)
 tt2<-land$tt
@@ -23,6 +24,8 @@ all$land<-ifelse(all$id.datetime %in% tt2, 1, 0)
 swim <- read.csv("swim.csv")
 swim<-dplyr::select(swim, ID:end.time)
 swim <- na.omit(swim)
+swim$start.datetime <- as.character(ymd_hms(paste(swim$start.date, swim$start.time))) #create column for datetime
+swim$end.datetime <- as.character(ymd_hms(paste(swim$end.date, swim$end.time)))
 
 swim$tt.start <- paste(swim$ID, swim$start.datetime) #temp start
 swim$tt.end <- paste(swim$ID, swim$end.datetime) #temp end
@@ -31,8 +34,16 @@ tt.end <- swim$tt.end
 all$start.swim <- ifelse(all$id.datetime %in% tt.start, 1,0)
 all$end.swim <- ifelse(all$id.datetime %in% tt.start, 1,0)
 
-## save df as .Rda
-save("all", file="mydata.RData")
+## save df as .RData
+save("all", file="pbears.RData")
+
+## Checking ####
+
+library(lubridate)
+test<-filter(all, start.swim==1)
+test2 <- filter(all, land==1)
+test3 <- filter(all, end.swim==1)
+
 
 
 ###########################
@@ -43,3 +54,11 @@ strptime(swim$Start.Date, "%m-%d-%Y %H:%M:%S") #Didn't work
 as.POSIXct(swim$Start.Date, tz="US/Alaska", sep="-") #Didn't work
 
 filter(all, animal=="pb_20845"& year==2015 & month==8 & day==27) #To check datetimes, because seconds (:ss) reset to 0 in Excel
+
+#resolving differences in land and test2 --> there are 6 different. Not sure why but not in OWS anyway. 
+test2.comp <- dplyr::select(test2, animal:gps_lon)
+land.comp<- dplyr::select(land, animal:gps_lon)
+dplyr::setdiff(land.comp, test2.comp)
+
+land<-dplyr::distinct(land) #no duplicates
+test2 <- dplyr::distinct(test2) #no duplicates
