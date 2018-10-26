991b34244b6c8a04c5ebd81668a6db0ac08014fb
Annie <annie.kellner@colostate.edu>
2018-10-26 16:56:15 -0600

    AKDT to UTC

--- rNOMADS_example.R
+++ rNOMADS_example.R
@@ -1,13 +1,21 @@
 rm(list = ls())
 library(rNOMADS)
-load("swim_ice.RData")
+library(dplyr)
+
+load("all.RData")
+swim <- subset(all, swim==1)
+swim <- arrange(swim, year)
+
+# Change date and time to UTC
+library(lubridate)
+swim$UTC <- with_tz(swim$datetime, "UTC")
 
 # example point
 lat <- 72.98459
 lon<- -159.3032
 
 #Find the latest Global Forecast System model run
-model.urls <- GetDODSDates("gfs_0p50")
+model.urls <- GetDODSDates("gfsanl")
 latest.model <- tail(model.urls$url, 1)
 model.runs <- GetDODSModelRuns(latest.model)
 latest.model.run <- tail(model.runs$model.run, 1)
