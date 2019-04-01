90c47492e15e606625aaa29786ec82376cc64435
Annie <annie.kellner@colostate.edu>
2019-03-31 21:39:31 -0800

    starting powerpoint in AK

--- Mig_time.R
+++ Mig_time.R
@@ -1,10 +1,16 @@
 rm(list = ls())
 library(dplyr)
 
-load('all.RData')
+load('all_v2.RData')
+
 start <- subset(all, start.swim==1)
+start <- start %>% # remove 'undecided'
+  filter(!(id=='pb_20333.2008' | id=='pb_20413.2006' | id=='pb_20418.2005' | id=='pb_20520.2012' | id=='pb_20529.2004'))
 start <- droplevels(start)
+
 end <- subset(all, end.swim==1)
+end <- end %>% # remove 'undecided'
+  filter(!(id=='pb_20333.2008' | id=='pb_20413.2006' | id=='pb_20418.2005' | id=='pb_20520.2012' | id=='pb_20529.2004'))
 end <- droplevels(end)
 
 mig <- left_join(start,end, by='id')
