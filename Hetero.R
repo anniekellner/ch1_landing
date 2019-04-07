3bb9074b09b93ac9ce7c614a9b56a6a5f75b9db1
Annie <annie.kellner@colostate.edu>
2019-04-07 15:09:25 -0800

    20414. Removed observations from 2009181 because MASIE data unavailable.

--- Hetero.R
+++ Hetero.R
@@ -10,14 +10,14 @@ rm(list = ls())
 
 library(dplyr)
 library(raster)
-library(sf)
 library(spData)
 library(lubridate)
 
-
-
 # create df with start dates
+
 load('all_v2.RData')
+load('Ice_Measurements.RData')
+
 start <- subset(all.v2, start.swim==1)
 start <- start %>% # remove 'undecided'
   filter(!(id=='pb_20333.2008' | id=='pb_20413.2006' | id=='pb_20418.2005' | id=='pb_20520.2012' | id=='pb_20529.2004'))
@@ -31,8 +31,12 @@ pb <- subset(all.v2, animal=='pb_20414' & datetime > '2009-06-17' & datetime < '
 pb$ordinal <- yday(pb$ymd) #change ymd to ordinal date
 pb$ord.year <- paste("2009", pb$ordinal, sep="")
 
+pb <- pb %>% #remove 2009181 because no data in MASIE
+  filter(!(ord.year == '2009181'))
+pb <- droplevels(pb)
+
 # create rasterstack using TIFs
-rasterlist <- list.files('./SIC-TIFs/MASIE/pb_06817', full.names = TRUE) # bring in all files
+rasterlist <- list.files('./SIC-TIFs/MASIE/pb_20414', full.names = TRUE) # bring in all files
 
 # separate date component of TIF name to correspond to spdf metadata 
 
@@ -75,17 +79,17 @@ for (i in 1:nrow(pb.spdf.polar)) {
   pb.spdf.polar$Buf500_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=500000, fun=mean, na.rm=T)}
   
   
-df.pb <- pb.spdf.polar@data #convert to df
+df.new <- pb.spdf.polar@data #convert to df
 
 # Change pct ice to pct water
-df.pb$pct.h20.10 <- 1-df.pb$Buf10_me
-df.pb$pct.h20.30 <- 1-df.pb$Buf30_me
-df.pb$pct.h20.50 <- 1-df.pb$Buf50_me
-df.pb$pct.h20.100 <- 1-df.pb$Buf100_me
-df.pb$pct.h20.500 <- 1-df.pb$Buf500_me
+df.new$pct.h20.10 <- 1-df.new$Buf10_me
+df.new$pct.h20.30 <- 1-df.new$Buf30_me
+df.new$pct.h20.50 <- 1-df.new$Buf50_me
+df.new$pct.h20.100 <- 1-df.new$Buf100_me
+df.new$pct.h20.500 <- 1-df.new$Buf500_me
 
-
-save(df.pb, file='Ice_Measurements.RData')
+df.new <- rbind(df.pb, df.new)
+save(df.new, file='Ice_Measurements.RData')
 
 ## cell is included if its center is covered by the buffer
 
