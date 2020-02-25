23d5e63e06927fea176084dca55206d2990e783f
anniekellner <annie.kellner@colostate.edu>
2020-02-24 23:46:39 -0700

    COde for error troubleshooting included. Had trouble writing to .shp using st_write

--- bat_pk_ice_poly.R
+++ bat_pk_ice_poly.R
@@ -7,6 +7,7 @@ rm(list = ls())
 library(raster)
 library(sf)
 
+
 # ----------- load data ----------------------------------------------#
 
 load('ded_ids.RData')
@@ -33,9 +34,27 @@ bat_pack_ice_poly <- function(rl){
     gv <- getValues(r) # change values to vector so can get mode
     mode <- modal(gv, na.rm=TRUE) # find mode
     poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
-    poly <- st_as_sf(poly) #convert to sf object
+     #convert to sf object
     poly <- st_transform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
-    st_write(poly, paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', rl[i]), driver='ESRI shapefile', update=TRUE)}}
+    st_write(poly, paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', rl[1]), driver='ESRI shapefile', update=TRUE, quiet = TRUE)}}
 
 #run the function
 bat_pack_ice_poly(rl)
+################################################################
+##    TROUBLESHOOTING
+
+r <- raster(paste0("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC/", rl[1]))
+gv <- getValues(r) # change values to vector so can get mode
+mode <- modal(gv, na.rm=TRUE) # find mode
+poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE)
+writeOGR(poly, dsn = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY", layer = rl[1], driver = 'ESRI Shapefile')
+
+
+r2 <- gsub(".", "-", r@data@names, fixed = TRUE)
+r2 <- sub("5-4", "5.4", r2)
+r@data@names <- r2
+
+poly <- st_transform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
+writeOGR(spdf.use,dsn = "C:/Users/akell/Documents/ArcGIS/Polar_Bears_GIS/Location Data", layer = "start_migration", driver = "ESRI Shapefile")
+
+st_write(poly, paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', rl[1]), driver='ESRI shapefile', delete_layer = TRUE)
