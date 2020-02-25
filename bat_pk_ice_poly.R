1ff35bb70b6eb6ec09db683e1d2f5d0e9a3b4b6b
anniekellner <annie.kellner@colostate.edu>
2020-02-24 21:12:15 -0700

    remove elements of character vector that have already been converted into shapefile

--- bat_pk_ice_poly.R
+++ bat_pk_ice_poly.R
@@ -11,25 +11,31 @@ library(sf)
 
 load('ded_ids.RData')
 
-rl <- dir(path = "./SIC-TIFs/SIC_univ_Bremen/RCC", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=FALSE)
+rl <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=FALSE)
+
+# remove elements as necessary
+as.list(rl)
+rl <- rl[-c(1:97)]
+
+as.character(rl)
 
 # Create directories for new polygons
 
-for(i in 1:length(ded)){
-dir.create(paste0('./SIC-TIFs/SIC_univ_Bremen/POLY/', ded[i]))
-}
+#for(i in 1:length(ded)){
+#dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', ded[i]))
+#}
 
 # ------------------- Convert rasters to polygons ------------------- #
 
 bat_pack_ice_poly <- function(rl){
   for (i in 1:length(rl)) {
-    r <- raster(paste0("./SIC-TIFs/SIC_univ_Bremen/RCC/", rl[i])) #read in raster
+    r <- raster(paste0("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC/", rl[i])) #read in raster
     gv <- getValues(r) # change values to vector so can get mode
     mode <- modal(gv, na.rm=TRUE) # find mode
     poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
     poly <- st_as_sf(poly) #convert to sf object
     poly <- st_transform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
-    st_write(poly, paste0('./SIC-TIFs/SIC_univ_Bremen/POLY/', rl[i]), driver='ESRI shapefile', delete_layer=TRUE)}}
+    st_write(poly, paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', rl[i]), driver='ESRI shapefile', update=TRUE)}}
 
 #run the function
 bat_pack_ice_poly(rl)
