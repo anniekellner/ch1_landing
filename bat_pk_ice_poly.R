1d864f9fb4629feaf9a4ed85c15ca8ea4cfeb392
anniekellner <annie.kellner@colostate.edu>
2020-03-02 12:44:36 -0700

    Nested loop appears to have created appropriate polygons in each folder. Next step is to see that the poly's are legit
    All polygons name polygon.shp. Directory indicates which dae the polygon refers to

--- bat_pk_ice_poly.R
+++ bat_pk_ice_poly.R
@@ -5,6 +5,7 @@
 rm(list = ls())
 
 library(raster)
+library(rgdal)
 library(sf)
 
 
@@ -12,31 +13,42 @@ library(sf)
 
 load('ded_ids.RData')
 
+ded <- ded[-c(1:3)] # remove folder names that already have polygons
+
 rl <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=FALSE)
 
 # remove elements as necessary
-as.list(rl)
-rl <- rl[-c(1:97)]
 
-as.character(rl)
+rl <- rl[-c(1:127)]
+rl2 <- str_remove(rl,".tif")
 
-# Create directories for new polygons
+rl3 <- character()
+for(i in 1:length(rl2)){
+  rl3[i] <- paste0(rl2[i], "/polygon")
+}
 
-#for(i in 1:length(ded)){
-#dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', ded[i]))
-#}
+ # paste0 collapses the string so there is no extra whitespace
+
+#Create directories for each shapefile
+for(i in 1:length(rl2)){
+dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY/', rl2[i]))
+}
 
 # ------------------- Convert rasters to polygons ------------------- #
 
 bat_pack_ice_poly <- function(rl){
   for (i in 1:length(rl)) {
+    for(j in 1:length(rl3)){
     r <- raster(paste0("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC/", rl[i])) #read in raster
     gv <- getValues(r) # change values to vector so can get mode
     mode <- modal(gv, na.rm=TRUE) # find mode
     poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
     spTransform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
-    writeOGR(poly, dsn = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY", layer = rl[1], driver = 'ESRI Shapefile')}}
-    
+    writeOGR(poly, dsn = 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/POLY', layer = rl3[j], driver = 'ESRI Shapefile')
+    }
+  }
+  }
+
 #run the function
 bat_pack_ice_poly(rl)
 
