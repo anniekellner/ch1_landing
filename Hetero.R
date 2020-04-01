2de8bdd8140c954102c562cf8c27c209fef99dc2
anniekellner <annie.kellner@colostate.edu>
2020-04-01 17:06:12 -0600

    Add row of missing data to ice.df
    Removed outdated analyses

--- Hetero.R
+++ Hetero.R
@@ -27,16 +27,17 @@ start <- droplevels(start)
 all.v2$ymd <- as.character(ymd(paste(all.v2$year, all.v2$month, all.v2$day))) #get ymd into POSIXct 
 all.v2$ymd <- as.POSIXct(all.v2$ymd, tz='US/Alaska')
 
-pb <- subset(all.v2, animal=='pb_32366' & datetime > '2014-07-13' & datetime < '2014-08-13') 
+pb <- subset(all.v2, animal=='pb_20525' & datetime > '2013-07-17' & datetime < '2013-08-18') 
 pb$ordinal <- yday(pb$ymd) #change ymd to ordinal date
-pb$ord.year <- paste("2014", pb$ordinal, sep="")
+pb$ord.year <- paste("2013", pb$ordinal, sep="")
+pb <- head(pb, -3) # remove rows after bear has started swimming
 
 #pb <- pb %>% #remove 2009181 because no data in MASIE
   #filter(!(ord.year == '2009181'))
 #pb <- droplevels(pb)
 
 # create rasterstack using TIFs
-rasterlist <- list.files('./SIC-TIFs/MASIE/pb_32366_2014', full.names = TRUE) # bring in all files
+rasterlist <- list.files('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/MASIE/pb_20525', full.names = TRUE) # bring in all files
 
 # separate date component of TIF name to correspond to spdf metadata 
 
@@ -68,33 +69,27 @@ pb.spdf.polar$Buf50_me<- NA
 pb.spdf.polar$Buf100_me <- NA
 pb.spdf.polar$Buf500_me <- NA
 
-
-# for loop that runs through each point and pulls data from appropriate GeoTIFF
-for (i in 1:nrow(pb.spdf.polar)) {
-  st3<-st2[[which(date==pb.spdf.polar$ord.year[i])]] #pulls raster data from GeoTIFF that corresponds to ordinal date
-  pb.spdf.polar$Buf10_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=10000, fun=mean, na.rm=T)
-  pb.spdf.polar$Buf30_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=30000, fun=mean, na.rm=T)
-  pb.spdf.polar$Buf50_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=50000, fun=mean, na.rm=T)
-  pb.spdf.polar$Buf100_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=100000, fun=mean, na.rm=T)
-  pb.spdf.polar$Buf500_me[i] <- extract(st3, pb.spdf.polar[i,], buffer=500000, fun=mean, na.rm=T)}
-  
-  
 df.new <- pb.spdf.polar@data #convert to df
 
 # Change pct ice to pct water
-df.new$pct.h20.10 <- 1-df.new$Buf10_me
-df.new$pct.h20.30 <- 1-df.new$Buf30_me
-df.new$pct.h20.50 <- 1-df.new$Buf50_me
-df.new$pct.h20.100 <- 1-df.new$Buf100_me
-df.new$pct.h20.500 <- 1-df.new$Buf500_me
+df.new$pct.h20.10 <- NA
+df.new$pct.h20.30 <- NA
+df.new$pct.h20.50 <- NA
+df.new$pct.h20.100 <- NA
+df.new$pct.h20.500 <- NA
 
-ice.df <- rbind(df.new, ice.df)
-ice.df <- droplevels(ice.df)
+# Add missing data to dataframe
+
+missing <- slice(df.new, n()) # select last row from pb dataframe (missing data 4/1/2020)
+
+ice.df <- rbind(ice.df, missing)
 save(ice.df, file='Ice_Measurements.RData')
 
 ## cell is included if its center is covered by the buffer
 
 ## The MASIE products are provided in a polar stereographic projection with the WGS 1984 datum
+
+
 
 
 
