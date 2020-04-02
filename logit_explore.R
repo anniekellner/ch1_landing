38867ee1c1324f81a7344281813d1705239a6e87
anniekellner <annie.kellner@colostate.edu>
2020-04-02 08:23:02 -0600

    Mean Patch Area works well - data looks good

--- logit_explore.R
+++ logit_explore.R
@@ -7,6 +7,7 @@ rm(list = ls())
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(zoo)
 
 # Load dataframe #
 
@@ -20,6 +21,7 @@ lsm$index <- as.numeric(lsm$index)
 
 lsm10 <- dplyr::filter(lsm, radius_m == 10000) # 10 km radius
 lsm30 <- dplyr::filter(lsm, radius_m == 30000) # 30 km radius
+lsm50 <- dplyr::filter(lsm, radius_m == 50000) # 50 km radius
 
 ###############################################################################
 ###     MEAN PATCH AREA   #####################################################
@@ -27,10 +29,10 @@ lsm30 <- dplyr::filter(lsm, radius_m == 30000) # 30 km radius
 ### 10 km Radius  ###
 
 area_mn_10 <- lsm10 %>%
-  filter(metric == "area_mn") %>%
-  filter(class == 3)
-
+  filter(metric == "area_mn") 
+ 
 head(area_mn_10)
+
 area_mn_10$id.datetime <- paste(area_mn_10$id, area_mn_10$datetime)
 
 area_mn_10 <- area_mn_10 %>% pivot_wider(names_from = c(class), values_from = c(class, value)) # spread rows into columns by class
@@ -42,7 +44,7 @@ full <- left_join(ice.df, area_mn_10, by = "id.datetime")# join ice.df with lsm 
 full$value_3[is.na(full$value_3)] <- 0
 full$class_3[is.na(full$class_3)] <- 3
 
-full$leave.ice <- ifelse(full$index==0,1,0)
+full$leave.ice <- ifelse(full$index==0,1,0) # day they depart = 1
 
 # logistic regression swim ~ value raw data
 fit <- glm(start.swim ~ value_3, data = full, family = binomial())
@@ -67,8 +69,8 @@ summary(fit2)
 ######   30 km radius
 
 area_mn_30 <- lsm30 %>%
-  filter(metric == "area_mn") %>%
-  filter(class == 3)
+  filter(metric == "area_mn") 
+  
 
 head(area_mn_30)
 area_mn_30$id.datetime <- paste(area_mn_30$id, area_mn_30$datetime)
@@ -109,6 +111,51 @@ ggplot(logreg, aes(mean_val, swim)) + geom_point() +
 par(mar = c(4,4,1,1))
 
 
+########  50 km radius
+
+### 50 km Radius  ###
+
+area_mn_50 <- lsm50 %>%
+  filter(metric == "area_mn") 
+
+
+head(area_mn_50)
+area_mn_50$id.datetime <- paste(area_mn_50$id, area_mn_50$datetime)
+
+area_mn_50 <- area_mn_50 %>% pivot_wider(names_from = c(class), values_from = c(class, value)) # spread rows into columns by class
+
+full <- left_join(ice.df, area_mn_50, by = "id.datetime")# join ice.df with lsm df's
+
+# Change ice value (3) to 0 if NA
+full$value_3[is.na(full$value_3)] <- 0
+full$class_3[is.na(full$class_3)] <- 3
+
+#full$leave.ice <- ifelse(full$index==0,1,0)
+
+# logistic regression swim ~ value raw data
+fit <- glm(start.swim ~ value_3, data = full, family = binomial())
+summary(fit) 
+
+# find means by days
+
+mean_val <- full %>% 
+  group_by(id.x, index) %>% 
+  summarise(mean_val = mean(value_3)) 
+
+sum_swim <- full %>%
+  group_by(id.x, index) %>%
+  summarize(swim = max(start.swim))
+
+logreg <- left_join(mean_val, sum_swim)   
+
+# Log. Reg. swim - averaged over 30 days
+fit2 <- glm(swim ~ mean_val, data = logreg, family = binomial())
+summary(fit2)
+
+ggplot(logreg, aes(mean_val, swim)) + geom_point() +
+  scale_x_reverse() +
+  stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE)
+
 ######  TROUBLESHOOTING     #################################################################################
 
 ## RESOLVED 04/01/2020
@@ -129,5 +176,12 @@ unique(nodata) # ANSWER: 20525.2013, 20414.2009, 20446.2009
 
 pb20525.ice <- subset(ice.df, animal=="pb_20525")
 
-
+#########################################################################################################
   
+# Problems - 20845.2015 - RESOLVED 4/2/2020
+# Make sure all rows have value_0 and value_3 before subsetting by value_3
+
+lsmtest <- subset(lsm, id=="pb_20845.2015") # lsm looks good 
+lsm10test <- subset(lsm10, id=="pb_20845.2015") # Problem was within code at this step= had filtered by 3's before all dates were accounted for
+areatest <- subset(area_mn_10, id=="pb_20845.2015")
+fulltest <- subset(full, id=="pb_20845.2015")
