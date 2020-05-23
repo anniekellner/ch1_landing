aa63d9706e2057759ebd459e3e2655feb72a8419
anniekellner <annie.kellner@colostate.edu>
2020-05-23 12:44:29 -0600

    Not working. Distance might be to center of shapefile? Need to figure out how to make distance to edge or closest point

--- Dist2land.R
+++ Dist2land.R
@@ -13,21 +13,21 @@ library(ggplot2)
 
 # LOAD DATA --------------------------------------------------------------------------------- #
 
-load('ice_calc.RData')
+load('land_bears_ows.RData')
 
-albers.proj <- CRS("+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") # Albers Equal Area Conic (same as ArcMap doc)
+polar.stereo <-CRS('+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0') 
 
 land <- st_read('C:/Users/akell/Documents/ArcGIS/Land Shapefiles/AK_CA_5kbuff.shp')
-land <- st_transform(land, albers.proj)
+land <- st_transform(land, polar.stereo)
 
 # Function to create sf object
 DFtoSF <- function(df) {
   sf.wgs <- st_as_sf(df, coords = c('gps_lon', 'gps_lat'), crs = 4326, dim = 'XY')
-  sf.alb <- st_transform(sf.wgs, albers.proj)
-  return(sf.alb)
+  sf.ps <- st_transform(sf.wgs, polar.stereo)
+  return(sf.ps)
 }
 
-pb <- DFtoSF(ice)
+pb <- DFtoSF(land.bears.all.ows)
 
 # plot to make sure projections match and points look good
 
@@ -40,6 +40,31 @@ ggplot() +
 dist <- vector()
 dist <- st_distance(pb, land, by_element = TRUE)
 
-ice$dist2land <- cbind(matrix(dist))
+pb$dist2land <- cbind(matrix(dist))
 
-save(ice, file = 'ice_calc.RData')
+save(pb, file = 'land_bears_ows.RData')
+
+# -------------------------------------------------------------------------- #
+
+# Verify
+
+# land polygon is invalid
+library(lwgeom)
+land <- st_make_valid(land)
+
+# check first ten points
+
+test <- pb[1:10,]
+test <- st_as_sf(test)
+
+library(tmap)
+tmap_mode("view")
+
+land.map <- tm_shape(land) +
+  tm_polygons() 
+
+land.map +
+tm_shape(test) + 
+  tm_dots()
+  
+
