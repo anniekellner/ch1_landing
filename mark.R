d121dd8284b7de11bcf3ab9bafb81990ead908b9
anniekellner <annie.kellner@colostate.edu>
2020-06-18 12:44:08 -0600

    Model worked running repro against model with no variability. Need to figure out what it means.

--- mark.R
+++ mark.R
@@ -5,6 +5,7 @@
 rm(list = ls())
 
 library(dplyr)
+library(RMark)
 
 
 # Add reproductive status 
@@ -16,6 +17,7 @@ library(dplyr)
 
 load('all_v2.RData')
 load('KFM.RData')
+data("Blackduck")
 
 # change reproductive status to categorical variable in all.v2
 
@@ -43,17 +45,54 @@ ind <- left_join(ch.all, repro)
 
 # Formatting for trial run via A. Dillon
 
-ind$group <- 1 
+#ind$group <- 1 
 
-ind$id2 <- paste0("/*", ind$id, "*/")
+#ind$id2 <- paste0("/*", ind$id, "*/")
 ind <- ungroup(ind)
 
-ind <- dplyr::select(ind, -id)
+ind <- dplyr::select(ind, -id) # dataframe must start with c
 
-ind <- select(ind, id2, ch, group, repro)
+#ind <- select(ind, id2, ch, group, repro)
 
-write.csv(ind, file = "test_run.csv") 
+#write.csv(ind, file = "test_run.csv") 
+
+# ----------------------------------------------------------------------------------------------------- #
+
+# Blackduck example
+
+Blackduck$BirdAge=as.numeric(Blackduck$BirdAge)-1
+bduck.processed = process.data(Blackduck, model = "Known")
+bduck.ddl=make.design.data(bduck.processed)
+bduck.ddl$S$min=c(4,6,7,7,7,6,5,5)
+
+S.dot=list(formula=~1) # standard model with no variability # estimating 1 param
+S.time=list(formula=~time) # different S for each occasion (year?) - now estimating 8 params
+S.min=list(formula=~min)
+S.BirdAge=list(formula=~BirdAge)
+
+S.BirdAgexWeight.min=list(formula=~min+BirdAge*Weight)
+S.BirdAge.Weight=list(formula=~BirdAge+Weight)
+#
+# Create model list and run assortment of models
 
 
 
+model.list=create.model.list("Known")
+bduck.results=mark.wrapper(model.list,data=bduck.processed,ddl=bduck.ddl, run = FALSE,
+                           invisible=FALSE,threads=1)
+bduck.results
 
+mydata <- ind
+
+# Process data
+mydata.processed <- process.data(mydata, model = "Known") # initializes variables, defines groups
+mydata.ddl <- make.design.data(mydata.processed)
+
+S.dot=list(formula=~1)
+S.Repro = list(formula=~repro)
+
+model.list=create.model.list("Known")
+
+mydata.results=mark.wrapper(model.list,data=mydata.processed,ddl=mydata.ddl)
+
+mydata.results
