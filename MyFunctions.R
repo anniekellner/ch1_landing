c5fef37d4982b7da4f727437f9f2cf0826b9a1cb
anniekellner <annie.kellner@colostate.edu>
2020-06-26 12:40:58 -0600

    Comparing SIC mean, min, max to see what is best to use

--- MyFunctions.R
+++ MyFunctions.R
@@ -4,9 +4,15 @@
 
 ## Functions used in Dissertation Chapter 1 ########
 
+#' Dataframe to SF
+#' 
+#' @param df Dataframe with lat/long (EPSG: 4326)
+#' @param projection what I want the data projected to
+
 DFtoSF <- function(df, projection) {
   df <- droplevels(df)
   sf.wgs <- st_as_sf(df, coords = c('gps_lon', 'gps_lat'), crs = 4326, dim = 'XY')
   sf.project <- st_transform(sf.wgs, projection)
   return(sf.project)
 }
+
--- SIC_explore_KFM.R
+++ SIC_explore_KFM.R
@@ -16,70 +16,53 @@ library(raster)
 
 # Load data
 
-load('all_v2.RData')
 load('KFM.RData')
 load('SIC_KFM.RData')
 
-ids <- unique(bears$id)
-pb <- subset(all.v2, id %in% ids)
-pb <- subset(pb, year == 2016 & month > 5 & month < 10) # no idea why the datetime needs to be set like this to eliminate entries from 2012-07-01
+SIC <- combine
+rm(combine)
 
-#create rasterstack using TIFs
-rasterlist <- list.files(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/OWS_2016", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=TRUE)
+# add variable for "on.ice"
+SIC$on.ice <- ifelse(SIC$swim == 1 | SIC$land == 1, 0, 1)
 
-# --------------------------------------------------------------------------------------------------- #
 
-# for loop for creating rasters
-# separate date component of TIF name to correspond to spdf metadata 
+# logistic regression - mean SIC 30km radius vs. land or ice
 
-stack<-list()
-date<-vector()
-for (i in 1:length(rasterlist)) {
-  stack[[i]]<-raster(rasterlist[i])
-  tt<-unlist(strsplit(names(stack[[i]]), '[[:punct:]]+')) #https://stackoverflow.com/questions/10738729/r-strsplit-with-multiple-unordered-split-arguments
-  date[i]<-tt[which(nchar(tt)==max(nchar(tt)))]
-  values(stack[[i]])[values(stack[[i]])>200]<-0
+fit_mean <- glm(on.ice ~ SIC_30m_me, data = SIC, family = binomial())
+summary(fit_mean)
+
+fit_max <- glm(on.ice ~ SIC_30m_max, data = SIC, family = binomial())
+summary(fit_max)
+
+fit_min <- glm(on.ice ~ SIC_30m_min, data = SIC, family = binomial())
+summary(fit_min)
+
+AIC(fit_mean, fit_max, fit_min)
+
+# Use S. Carver's code to create AIC tables for myself (see below)
+
+AIC <- function(model.list, model.names) {
+  K=c(NA)
+  neg2logL=c(NA)
+  aic=c(NA)
+  for(i in 1:length(model.list)){
+    K[i]=length(coef(model.list[[i]]))#K
+    neg2logL[i]=-2*logLik(mods[[i]])#-2Log(L)
+    aic[i]=AIC(mods[[i]])#AIC
+  }
+  deltaAIC=aic-min(aic)
+  weight=exp(-1/2*deltaAIC)/sum(exp(-1/2*deltaAIC))
+  weightpct=weight*100
+  table1=cbind(1:63,K,neg2logL,aic,deltaAIC,weight,weightpct)
+  colnames(table1)=c("Model","K","-2LOG(L)","AIC","deltaAIC","Weight","Weight (%)")
+  rownames(table1)=model.names
+  table1=table1[order(table1[,4]),]
+  table1=data.frame(table1)
+  return(table1)
 }
 
-####################################################################
-######## create SpatialPointsDataFrame for locations ########
-
-## AMSR-E 1 June 2002 - 4 Oct 2011
-## AMSR-2 July 2012 - 17 Nov 2018
-
-#all$ymd <- as.character(ymd(paste(all$year, all$month, all$day))) #get ymd into POSIXct so can be subsetted
-#all$ymd <- as.POSIXct(all$ymd, tz='US/Alaska')
 
 
-projection <- CRS("+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") #find this in spatialreference.org
-polar.stereo <-crs('+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs') # epsg projection 3411 NSIDC sea ice polar stereographic north
-coords <- cbind(pb$X, pb$Y)
-pb.spdf <- SpatialPointsDataFrame(coords = coords, data = pb, proj4string = projection) 
-pb.spdf2 <-spTransform(pb.spdf, polar.stereo) #reproject points to polar stereographic
-pb.spdf2$date2 <- format(pb.spdf2$ymd, "%Y%m%d")
-
-
-# for loop that runs through each point and pulls data from appropriate GeoTIFF
-
-for (i in 1:nrow(pb.spdf2)) {
-  st<-stack[[which(date==pb.spdf2$date2[i])]]
-  pb.spdf2$SIC_30m_me[i]<-extract(st, pb.spdf2[i,], buffer = 30000, fun = mean, na.rm = TRUE)
-  pb.spdf2$SIC_30m_max[i]<-extract(st, pb.spdf2[i,], buffer = 30000, fun = max, na.rm = TRUE)
-  pb.spdf2$SIC_30m_min[i]<-extract(st, pb.spdf2[i,], buffer = 30000, fun = min, na.rm = TRUE)
-  }
-
-
-
-current <- pb.spdf2@data #convert to df
-#df <- dplyr::select(df, -date2)
-
-combine <- rbind(combine, current)
-
-head(combine)
-tail(combine)
-
-save(combine, file='SIC_KFM.RData')
-
-setdiff(ids, unique(combine$id)) # pb_21238.2012 is missing from SIC df. Probably only had points in June 2012. Ignore.
-
-
+ggplot(logreg, aes(mean_val, swim)) + geom_point() +
+  scale_x_reverse() +
+  stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE)
