0069751aab37a576b86e0389c7e7fa9d772ff506
anniekellner <annie.kellner@colostate.edu>
2020-06-27 11:29:03 -0600

    Distance to land for all bears

---
+++ logreg_Distance.R
@@ -0,0 +1,79 @@
+######################################################################################
+##########      DISTANCE DATA FOR KFM     ############################################
+######################################################################################
+
+# Compile Distance-to-shore and Distance-to-ice pack data for all bears in analysis
+
+rm(list = ls())
+
+library(sf)
+library(sp)
+library(dplyr)
+library(tmap)
+library(lwgeom)
+library(proj4)
+library(raster)
+
+# ----  BEAR SPATIAL DATA ---------------------------------------------------------------------------------------------------- #
+
+# Load bear data
+
+load("logreg.RData")
+logreg <- SIC; rm(SIC)
+
+polar.stereo <-CRS('+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0') 
+
+DFtoSF <- function(df, projection) {
+  sf.wgs <- st_as_sf(df, coords = c('gps_lon', 'gps_lat'), crs = 4326, dim = 'XY')
+  sf.project <- st_transform(sf.wgs, projection)
+  return(sf.project)
+}
+
+logreg <- DFtoSF(logreg, polar.stereo)
+
+
+# ------- LAND SPATIAL DATA  ---------------------------------------------------------------------------------------------- #
+
+setwd('C:/Users/akell/Documents/ArcGIS')
+
+bbox <- st_read('./Land Shapefiles/bbox.shp')
+
+land <- st_read('./Land Shapefiles/AK_CA_5kbuff.shp')
+land.dissolve <- land %>% summarise(Shape_Area = sum(Shape_Area)) # dissolve AK and territories into one obj
+plot(st_geometry(land.dissolve)) # OK
+
+# see where bbox overlaps shapefile
+
+tm_shape(land.dissolve) +
+  tm_polygons() +
+  tm_shape(bbox) +
+  tm_polygons()
+
+land.crop <- st_crop(land.dissolve, xmin = -596311, ymin = 2083889, xmax = 850962, ymax = 3576142) # crop shapefile
+land.ps <- st_transform(land.crop, polar.stereo)
+
+# Verify land and points look good
+
+tm_shape(logreg) + # plot points first so frame is large enough to accompany points and land
+  tm_dots() +
+  tm_shape(land.ps) +
+  tm_fill()
+
+# Check bear points in Chukchi Sea
+#THey are early June, bear pb_20797. Leaving in because I doubt it will make a difference and not worth removing
+
+tmap_mode("view")
+
+tm_shape(logreg) +
+  tm_dots(popup.vars = c("id", "ymd")) +
+  tm_shape(land.ps) +
+  tm_fill()
+
+# ---------   DISTANCE ANALYSIS   ------------------------------------------ #
+
+dist <- vector()
+dist <- st_distance(logreg, land.ps, by_element = TRUE)
+
+logreg$dist2land <- cbind(matrix(dist))
+
+save(logreg, file = "C:/Users/akell/OneDrive - Colostate/PhD/Polar_Bears/Repos/ch1_landing/logreg.RData")
