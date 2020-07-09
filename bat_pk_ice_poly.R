fd7f0157d5dcf73089765bbebae9ddde06d03f51
anniekellner <annie.kellner@colostate.edu>
2020-07-09 17:15:39 -0600

    removed [j] and it seems to work!

--- bat_pk_ice_poly.R
+++ bat_pk_ice_poly.R
@@ -38,16 +38,15 @@ dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_un
 
 bat_pack_ice_poly <- function(rl, rl2){
   for (i in 1:length(rl)) {
-    for(j in 1:length(rl2)){
     r <- raster(paste0("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/All/RCC/", rl[i])) #read in raster
     gv <- getValues(r) # change values to vector so can get mode
     mode <- modal(gv, na.rm=TRUE) # find mode
     poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
-    spTransform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
-    writeOGR(poly, dsn = 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/POLY', layer = rl2[j], driver = 'ESRI Shapefile')
+    poly <- spTransform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
+    writeOGR(poly, dsn = 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/POLY', layer = rl2[i], driver = 'ESRI Shapefile')
     }
   }
-  }
+  
 
 #run the function
 bat_pack_ice_poly(rl, rl2)
