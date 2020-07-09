81a26620882f5f38e381414fc38605e95a759a14
Annie <annie.kellner@colostate.edu>
2020-07-08 18:10:20 -0600

    wind speed NARR is most significant. Next: do wind direction

--- MyFunctions.R
+++ MyFunctions.R
@@ -22,11 +22,24 @@ DFtoSF <- function(df, projection) {
 #' @param aicc A dataframe comparing models with AICc results calculated using package MuMin
 
 create_AICc_table <- function(aicc){
+  aicc$Model <- rownames(aicc)
   aicc$deltaAICc <- aicc$AICc - min(aicc$AICc) 
   aicc$L <- exp(-0.5*(aicc$deltaAICc))
   aicc$weight <- aicc$L/(sum(aicc$L))
   aicc$weight.pct <- aicc$weight*100
   aicc <- arrange(aicc, aicc$deltaAICc)
   return(aicc)
+}
+
+#' Calculate wind direction (degrees)
+#' https://stackoverflow.com/questions/8673137/calculating-wind-direction-from-u-and-v-components-of-the-wind-using-lapply-or-i
+#' 
+#' @param u U vector
+#' @param v V Vector
+
+windDir <- function(u, v) {
+  if(v > 0)         ((180 / pi) * atan(u/v) + 180)
+  if(u < 0 & v < 0) ((180 / pi) * atan(u/v) + 0)
+  if(u > 0 & v < 0) ((180 / pi) * atan(u/v) + 360)
 }
 
