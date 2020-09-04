7ad0b882d4d926dd4aa190a82768eb813a0baff1
anniekellner <annie.kellner@colostate.edu>
2020-09-04 14:04:19 -0600

    conducted visual assessment of ice bears and removed ones with insufficient data

--- Departure_date_ice.R
+++ Departure_date_ice.R
@@ -12,12 +12,44 @@ load('logreg.RData')
 
 # ----  Data Prep  ------------------ #
 
-icebears <- subset(all.v2, land_bear == 0)
-icebears67 <- subset(icebears, month == 6 | month == 7)
+# Remove bears for which there is little data
+
+ice <- all.v2 %>%
+  filter(month == 6 | month == 7) %>%
+  filter(land_bear == 0) %>%
+  select(animal:datetime, id)
 
 
+noData <- ice %>% # remove bears with < 100 data points
+  group_by(id) %>%
+  add_count(id) %>%
+  filter(n < 50)
 
-# ----- Visualize ------ #
+ice <- anti_join(ice, noData)
+
+ice.sf <- inner_join(logreg, ice) # include land_bear designation
+
+
+# Visualize
+
+tmap_mode("view")
+
+tm_shape(ice.sf) + 
+  tm_symbols(col = "month", popup.vars = "datetime")  + 
+  tm_facets(by = "id") +
+  tmap_options(limits = c(facets.view = 100))
+
+# Bears with insufficient data via visual inspection
+
+ice.sf <- ice.sf %>%
+  filter(id != "pb_20794.2005") %>%
+  filter(id != "pb_20925.2009") %>%
+  filter(id != "pb_21221.2010") %>%
+  filter(id != "pb_21283.2012") %>% 
+  filter(id != "pb_21291.2012") %>%
+  filter(id != "pb_21296.2012") %>%
+  filter(id != "pb_21309.2012") %>%
+  filter(id != "pb_32799.2006")
 
 
 
