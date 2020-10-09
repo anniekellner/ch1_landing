5ed8650df70685c828dffd6e867d3f1b58961fa3
anniekellner <annie.kellner@colostate.edu>
2020-10-08 19:52:07 -0600

    submitted request to movebank for 20333 and 20525.2014

--- movebank.R
+++ movebank.R
@@ -10,10 +10,11 @@ library(sf)
 
 load('land_bears_CoxPH.RData') 
 new <- subset(bears, id == "pb_20333.2008" | id == "pb_20525.2014")
+new <- distinct(new)
 
 # select columns
 
-movebank <- dplyr::select(bears, datetime, gps_lon, gps_lat, id)
+movebank <- dplyr::select(new, datetime, gps_lon, gps_lat, id)
 head(movebank)
 
 colnames(movebank) <- c('timestamp', 'location-long', 'location-lat', 'id')
@@ -21,12 +22,14 @@ colnames(movebank) <- c('timestamp', 'location-long', 'location-lat', 'id')
 tz(movebank$timestamp) # should be US/Alaska
 
 movebank$timestamp <- with_tz(movebank$timestamp, "UTC") # Change date and time to UTC
-movebank$timestamp <- paste0(movebank$timestamp, ".00") # required movebank formatting
+#movebank$timestamp <- paste0(movebank$timestamp, ".00") # required movebank formatting
 
 movebank$ht.above.ellipsoid <-0 #need to change '.' to '-' in .csv
 
 movebank <- movebank[, c(1:3, 5,4)] # reorder columns to fit Movebank format
 head(movebank)
 
+movebank$timestamp <- ymd_hms(movebank$timestamp)
 
-write.table(movebank, 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/Movebank_10082020.csv', row.names = FALSE, sep = ",")
+
+write.csv(movebank, 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/Movebank_10082020.csv', row.names = FALSE)
