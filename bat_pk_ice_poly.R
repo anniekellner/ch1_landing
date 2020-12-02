f1e3085f7876650330aac34570a31ddedc328f97
anniekellner <annie.kellner@colostate.edu>
2020-12-02 12:38:02 -0700

    converted missing rasters into pack ice shapefiles.

--- bat_pk_ice_poly.R
+++ bat_pk_ice_poly.R
@@ -15,10 +15,10 @@ library(stringr)
 #load('ded_ids.RData')
 #ded <- ded[-c(1:3)] # remove folder names that already have polygons
 
-rl <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/All/RCC", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=FALSE)
+rl <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/rasters/RCC", pattern='.tif', all.files=TRUE, recursive = TRUE, full.names=FALSE)
 
 # remove elements as necessary
-rl <- rl[-c(1:767)]
+#rl <- rl[-c(1:767)]
 
 rl2 <- str_remove(rl,".tif")
 
@@ -31,39 +31,39 @@ for(i in 1:length(rl2)){
 
 #Create directories for each shapefile
 for(i in 1:length(rl2)){
-dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/POLY/', rl2[i]))
+dir.create(paste0('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/shapefiles/new', rl2[i]))
 }
 
 # ------------------- Convert rasters to polygons ------------------- #
 
-bat_pack_ice_poly <- function(rl, rl2){
+bat_pack_ice_poly <- function(rl){
   for (i in 1:length(rl)) {
-    r <- raster(paste0("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/All/RCC/", rl[i])) #read in raster
+    r <- raster(paste0("./rasters/RCC/", rl[i])) #read in raster
     gv <- getValues(r) # change values to vector so can get mode
     mode <- modal(gv, na.rm=TRUE) # find mode
     poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
     poly <- spTransform(poly, '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m + datum=WGS84 +no_defs +towgs84=0,0,0')
-    writeOGR(poly, dsn = 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/POLY', layer = rl2[i], driver = 'ESRI Shapefile')
+    writeOGR(poly, dsn = 'C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/shapefiles/new', layer = rl[i], driver = 'ESRI Shapefile')
     }
   }
   
-
+r <- raster(paste0("./rasters/", rl[i]))
 #run the function
-bat_pack_ice_poly(rl, rl2)
+bat_pack_ice_poly(rl)
+
 
 # -------------------------------------------------------------------------------------------------------- #
 # Test the script - make sure the polygon looks like the raster. 
 #Pairwise assessment to make sure R1-R2 are different; R1-P1 similar; R2-P2 similar
 
 
-setwd('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen')
-
-
-r1 <- raster('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/RCC/pb_20525.2013/asi-AMSR2-n6250-20130717-v5.4.tif')
-p1 <- st_read('./POLY/pb_20525.2013/asi-AMSR2-n6250-20130719-v5.4/polygon.shp')
+r1 <- raster('C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/rasters/RCC/asi-AMSR2-n3125-20130814-v5.4.tif')
+p1 <- st_read("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/shapefiles/new/asi-AMSR2-n3125-20130814-v5.4.tif.shp")
 
 plot(r1)
 plot(st_geometry(p1))
+
+# Looks good!
 
 r2 <- raster('./RCC/pb_20529.2005/asi-n6250-20050820-v5.4.tif')
 p2 <- st_read('./POLY/pb_20529.2005/asi-n6250-20050820-v5.4/polygon.shp')
