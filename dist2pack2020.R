b4754ebc2b36ce678c793997540ea4fd7b27480d
anniekellner <annie.kellner@colostate.edu>
2020-12-03 17:43:05 -0700

    calculated distance to pack ice from departure points

--- dist2pack2020.R
+++ dist2pack2020.R
@@ -1,6 +1,6 @@
-#################################################
-###   DISTANCE TO THE ICE PACK    ###############
-#################################################
+######################################################################
+###   DISTANCE TO THE ICE PACK FROM DEPARTURE POINT   ###############
+######################################################################
 
 
 rm(list = ls())
@@ -10,69 +10,72 @@ library(sp)
 library(sf)
 library(rgdal)
 library(dplyr)
+library(lubridate)
 
 # load data
 
-load('logreg.RData') #GPS data
-filelist <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/POLY", pattern='.shp', all.files=TRUE, recursive = TRUE, full.names=TRUE)
-pb <- subset(logreg, id=='pb_06336.2004') # test with one bear
+load('coxph.RData') #GPS data
+swim <- subset(bears, start.swim == 1)
+swim <- distinct(swim)
+filelist <- dir(path = "C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/shapefiles", pattern='.shp', all.files=TRUE, recursive = TRUE, full.names=TRUE)
+#pb <- subset(swim, id=='pb_06817.2006') # test with one bear
 
 # Create spatial object. This works - always do it this way!
 
 projection <- CRS("+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") #find this in spatialreference.org
 polar.stereo <-CRS('+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs') # matches AMSRE raster
-coords <- cbind(pb$X, pb$Y)
-pb.spdf <- SpatialPointsDataFrame(coords = coords, data=pb, proj4string = projection) 
+coords <- cbind(swim$X, swim$Y)
+pb.spdf <- SpatialPointsDataFrame(coords = coords, data=swim, proj4string = projection) 
 pb.spdf.polar <-spTransform(pb.spdf, polar.stereo)
-pb.spdf.polar$date2 <- format(pb.spdf.polar$ymd, "%Y%m%d") # format for matching up with dates on GeoTIFFs
+pb.spdf.polar$date2 <- gsub("-", "", pb.spdf.polar$ymd) # format for matching up with dates on GeoTIFFs
+swim.sf <- st_as_sf(pb.spdf.polar)
 
-track <- st_as_sf(pb.spdf.polar) # as sf object 
-plot(st_geometry(track))  # verify track looks good
+#### CHECK WITH ONE BEAR TO SEE WHETHER PROCESS LOOKS GOOD #######
+
+#track <- st_as_sf(pb.spdf.polar) # as sf object 
+#plot(st_geometry(track))  # verify track looks good
 
 
 # Test projection compatibility using one shapefile
 
-shp <- st_read(filelist[1500])
+shp <- st_read(filelist[2])
+points <- st_transform(swim.sf, st_crs(shp)) # transform track to shp projection
+
 plot(st_geometry(shp))
-#plot(st_geometry(track), add = TRUE)
+plot(st_geometry(points), add = TRUE)
+
+library(tmap)
+
+tmap_mode("view")
+
+tm_shape(shp) +
+  tm_borders() + 
+  tm_shape(points) + 
+  tm_dots()
+
+# looks good on map
 
 # Make sure distance calculation works
 
-#disttest <- st_distance(track[1,], shp) # works!
-# if crs(x) doesn't equal crs(y):
-#st_crs(shp) <- st_crs(track)
-#shp <- st_transform(shp, polar.stereo)
-
+#disttest <- st_distance(track, shp) # works!
 
 # ---------   DISTANCE ANALYSIS   ------------------------------------------ #
 
 # Associate GPS point with polygon file name
 
 dist <- vector()
-for (i in 1:nrow(track)){
-  file <- filelist[grep(track$date2[1], filelist)]
+for (i in 1:nrow(points)){
+  file <- filelist[grep(points$date2[i], filelist)]
   shp <- st_read(file)
-  st_crs(shp) <- st_crs(track)
-  shp <- st_transform(shp, polar.stereo)
-  dist[i] <- st_distance(track[i,], shp)
+  points <- st_transform(points, st_crs(shp))
+  dist[i] <- st_distance(points[i,], shp)
   }
 
+points$dist2pack <- dist
 
+# ----- STATS ------------------------------------------------------------- #
 
-pb$dist2pack <- cbind(matrix(dist)) # transposes vector into a column and binds to original subsetted df
-head(pb) 
-tail(pb)
-
-tt <- rbind(tt, pb) # create new df via rbind
-
-           
-            
-# ------------------------------------------------------------------------------------------- #
-
-save(tt, file='ice_calc.RData') # rewrite existing df with new data
-# ------------------------------------------------------------------ #
-
-# paste0 collapses the string so there is no extra whitespace
+mean(points$dist2pack)
 
 
 
