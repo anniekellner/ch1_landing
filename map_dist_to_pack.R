3da0246d4a5ccc01abb187c292ea432cc4120ad3
anniekellner <annie.kellner@colostate.edu>
2020-12-05 13:56:37 -0700

    code works for max raster

--- map_dist_to_pack.R
+++ map_dist_to_pack.R
@@ -30,12 +30,25 @@ load("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n31
 
 min <- st_as_stars(pack[[1]])
 min <- na_if(min, "FALSE") # eliminate ice not connected to pack
+min <- st_transform(min, st_crs(swim.sf))
 
 max <- st_as_stars(pack[[14]])
 max <- na_if(max, "FALSE") # eliminate ice not connected to pack
-
-min <- st_transform(min, st_crs(swim.sf))
 max <- st_transform(max, st_crs(swim.sf))
+
+
+q25 <- st_as_stars(pack[[11]])
+q25 <- na_if(q25, "FALSE")
+q25 <- st_transform(q25, st_crs(swim.sf))
+
+q50 <- st_as_stars(pack[[9]])
+q50 <- na_if(q50, "FALSE")
+q50 <- st_transform(q50, st_crs(swim.sf))
+
+q75 <- st_as_stars(pack[[14]])
+q75 <- na_if(q75, "FALSE")
+q75 <- st_transform(q75, st_crs(swim.sf))
+
 
 # Bounding box
 bb.swim <- tmaptools::bb(swim.sf, width = 2, height = 3, relative = TRUE)
@@ -50,6 +63,11 @@ library(rnaturalearthdata)
 nor_america <- ne_countries(continent = 'north america', returnclass = 'sf')
 nor_america <- st_transform(nor_america, st_crs(swim.sf))
 
+tm_shape(max, bbox = bb.swim2) + 
+  tm_raster() + 
+  tm_shape(swim.sf, bbox = bb.swim2) +
+  tm_dots()
+
 tm_shape(min, bbox = bb.swim2) + # this works with min raster
   tm_raster(col = "#7991FA", alpha = 0.5, legend.show = FALSE) + 
   tm_shape(max, bbox = bb.swim2) + 
@@ -60,7 +78,7 @@ tm_shape(min, bbox = bb.swim2) + # this works with min raster
   tm_fill(col = "#9CD3AA")
 
 tm_shape(max, bbox = bb.swim2) + # this one works with max raster
-  tm_raster() + 
+  tm_raster(col = "#edf8fb", legend.show = FALSE) + 
   tm_shape(swim.sf) +
   tm_dots() + 
   tm_shape(nor_america) + 
