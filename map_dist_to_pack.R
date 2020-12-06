2a03af910b67b9d11fa91b1fa9df224eb28744aa
Annie <annie.kellner@colostate.edu>
2020-12-05 20:42:06 -0700

    map is working using shapefiles instead of rasters

--- map_dist_to_pack.R
+++ map_dist_to_pack.R
@@ -10,10 +10,10 @@ library(stars)
 library(tmap)
 library(raster)
 
-setwd("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim")
-
-
 load('coxph.RData') #GPS data
+
+#setwd("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim") # Home Computer
+setwd("D:/Polar Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim")
 
 swim <- subset(bears, start.swim == 1)
 swim <- distinct(swim)
@@ -31,43 +31,20 @@ plot(st_geometry(swim.sf))
 
 load("C:/Users/akell/Documents/PhD/Polar_Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/rasters.RData")
 
+max <- sf::st_read('./shapefiles/asi-n3125-20090718-v5.4.tif.shp')
+max <- sf::st_transform(max, sf::st_crs(swim.sf))
 
-# Stats 
+# Min ice extent
 
-size <- vector()
-
-for(i in 1:length(pack)){
-  size[i] <- cellStats(pack[[i]], 'sum')
-}
-
-quantile(size)
-
-size_sort <- sort(size)
-
-
-# minimum raster
-
-min_ras <- raster('./rasters/ice_binary/asi-AMSR2-n3125-20120815-v5.4.tif')
-gv <- getValues(min_ras)
-mode <- modal(gv, na.rm = TRUE) # 526
+#r <- raster('D:/Polar Bears/Data/SIC-TIFs/SIC_univ_Bremen/n3125/start_swim/rasters/RCC/asi-AMSR2-n3125-20120815-v5.4.tif')
+#gv <- getValues(r) # change values to vector so can get mode
+#mode <- modal(gv, na.rm=TRUE) # find mode
+#poly <- rasterToPolygons(r, function(x){x==mode}, dissolve = TRUE) #raster to polygon
+#writeOGR(poly, dsn = './shapefiles', layer = r, driver = 'ESRI Shapefile')
+  
 
 
-m <- c(0,526,0,526,527,1, 527,Inf,0)
-rclmat <- matrix(m, ncol=3, byrow=TRUE)
 
-rc_min <- reclassify(min_ras, rclmat) 
-rcc_min <- clump(rc_min, directions=8)
-
-# maximum raster
-
-max_ras <- raster('./rasters/ice_binary/asi-n3125-20090718-v5.4.tif')
-gv <- getValues(max_ras)
-mode <- modal(gv, na.rm = TRUE) # 125
-
-m <- c(0,125,0, 125,126,1, 126,100000,0) 
-rclmat <- matrix(m, ncol=3, byrow=TRUE)
-
-rc_max <- reclassify(max_ras, rclmat) 
 
 # Median raster
 
@@ -80,21 +57,7 @@ rclmat <- matrix(m, ncol=3, byrow=TRUE)
 
 rc_med <- reclassify(med_ras, rclmat) 
 
-all_ras <- rc_min + rc_max
-plot(all_ras)
 
-
-test <- pack[[1]] + pack[[14]]
-
-
-#rclmat <- matrix(m, ncol=2, byrow=TRUE)
-
-#for(i in 1:length(pack)){
-  #new <- reclassify()
-
-min <- st_as_stars(pack[[1]])
-min <- na_if(min, "FALSE") # eliminate ice not connected to pack
-#min <- st_transform(min, st_crs(swim.sf))
 
 max <- st_as_stars(pack[[14]])
 max <- na_if(max, "FALSE") # eliminate ice not connected to pack
@@ -114,11 +77,7 @@ q75 <- st_as_stars(pack[[5]])
 q75 <- na_if(q75, "FALSE")
 #q75 <- st_transform(q75, st_crs(swim.sf))
 
-# Combine rasters
 
-
-
-test <- recode(stack, 1 == "TRUE")
 
 # Make map
 
@@ -126,40 +85,23 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 
 nor_america <- ne_countries(continent = 'north america', returnclass = 'sf')
-nor_america <- st_transform(nor_america, st_crs(swim.sf))
+nor_america <- sf::st_transform(nor_america, sf::st_crs(swim.sf))
 
 
 # Bounding box
 bb.swim <- tmaptools::bb(swim.sf, width = 2, height = 3, relative = TRUE)
-bb.swim2 <- tmaptools::bb(bb.swim, ylim = c(0.2, 2), relative = TRUE)
 
-tm_shape(swim.sf, bbox = bb.swim2) + # test whether raster works individually
+bb.swim2 <- tmaptools::bb(bb.swim, ylim = c(0.23, 2), relative = TRUE)
+
+tm_shape(swim.sf, bbox = bb.swim2) + # works
   tm_dots() + 
+  tm_shape(nor_america) + 
+  tm_fill('#9CD3AA') +
   tm_shape(min) + 
-  tm_raster()
-
-tm_shape(swim.sf, bbox = bb.swim2) + 
-  tm_dots() + 
-  tm_shape(nor_america) +
-  tm_fill(col = "#9CD3AA")
-
-
-tm_shape(swim.sf, bbox = bb.swim2) + 
-  tm_dots() +
-  #tm_shape(nor_america) + 
-  #tm_fill(col = "#9CD3AA") + 
+  tm_borders(col = "#810f7c") + 
   tm_shape(max) + 
-  tm_raster(col = "#edf8fb", legend.show = FALSE) + 
-  tm_shape(min, bbox = bb.swim2) + 
-  tm_raster(col = "#810f7c", legend.show = FALSE)
-
-
-
-
-tm_shape(max, bbox = bb.swim2) + 
-  tm_raster() + 
-  tm_shape(swim.sf, bbox = bb.swim2) +
-  tm_dots()
+  tm_borders(col = "#edf8fb")
+  
 
 
   
