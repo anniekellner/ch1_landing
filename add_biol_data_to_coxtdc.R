0f7e2e6c8d13c4e08994db462e77f53c9628ed10
anniekellner <annie.kellner@colostate.edu>
2021-01-11 14:24:25 -0700

    biological variables do not affect timing of migration

--- add_biol_data_to_coxtdc.R
+++ add_biol_data_to_coxtdc.R
@@ -3,13 +3,23 @@
 ######################################################
 
 library(dplyr)
+library(lubridate)
+library(survival)
+library(MuMIn)
 
 
 rm(list = ls())
 
 # Prep data
+# Land bear df
 
-load('Cox_TDC.RData')
+load("land_bears_CoxPH.RData")
+
+bears <- bears %>%
+  select(id, animal:gps_lon, datetime, land, start.swim, X, Y, ymd, go_into_den:yearling)
+
+# Biological vars from .csv (T. Atwood)
+
 bio1 <- read.csv('./data/raw-data/resid_mass_indfem_19832015.csv')
 bio2 <- read.csv('./data/raw-data/Residual_body_masses_additional_bears_20210102.csv')
 
@@ -30,26 +40,54 @@ colnames(bio2) <- c("animal", "ResidualMass", "age", "id")
 
 biolog <- rbind(bio1, bio2)
 
-cox_tdc <- left_join(cox_tdc, biolog)
+# ------------------------------------------------------------------------------------ #
 
-cox_tdc %>%
-  group_by(id) %>%
-  slice_head()
+# Repro data 
 
-missing <- subset(bio, animal == "pb_20845") # no residmass but other measurements present - 20446
-# 6817, 20333 (2008), 20418 (2005), 20520 (2012), 20529 (2005), 20735 (2009), 21264 (2011), 21358 (2013) 
+bears$repro <- 0
 
+bears <- bears %>%
+  mutate(repro = replace(repro, go_into_den == 1, 1)) %>%
+  mutate(repro = replace(repro, coy == 1, 2)) %>%
+  mutate(repro = replace(repro, yearling == 1, 3))
+
+bears$repro <- factor(bears$repro, labels = c("Unknown", "Enter_Den", "COY", "Yearling"))
+
+bears <- left_join(bears, biolog)
+
+# ---------------------------------------------------------------------------------------- #
+
+# Coxph analysis with biological variables
+
+source('MyFunctions.R') # AICC
+
+bears <- bears %>% 
+  group_by(id, ymd) %>%
+  mutate(ordinal = yday(ymd)) %>%
+  dplyr::summarise(
+    first(animal), first(ordinal), max(start.swim), first(repro), first(age), first(ResidualMass), max(start.swim)
+  )
+
+colnames(bears) <- c("id", "ymd", "animal", "ordinal", "start.swim", "repro", "age", "resid_mass")
+
+# --  RUN MODELS ----------------------------------------------------------------------------------------------- #
+
+repro <- coxph(Surv(ordinal, start.swim) ~ repro, data = bears)
+age <- coxph(Surv(ordinal, start.swim) ~ age, data = bears)
+rm <- coxph(Surv(ordinal, start.swim) ~ resid_mass, data = bears)
+repro_age <- coxph(Surv(ordinal, start.swim) ~ repro + age, data = bears)
+repro_rm <- coxph(Surv(ordinal, start.swim) ~ repro + resid_mass, data = bears)
+age_rm <- coxph(Surv(ordinal, start.swim) ~ age + resid_mass, data = bears)
+repro_age_rm <- coxph(Surv(ordinal, start.swim) ~ repro + age + resid_mass, data = bears)
+
+aicc <- AICc(repro, age, rm, repro_age, repro_rm, age_rm, repro_age_rm)
+
+create_AICc_table(aicc)
+
+summary(age)
+
+# CONCLUSION: No biological variables affect the TIMING of migration 
 
 # ------------------------------------------------------------------------------------------------------- #
  # COX REGRESSION WITH BIOLOGICAL VARIABLES #
-
-create_AICc_table <- function(aicc){ # AICc table function
-  aicc$Model <- rownames(aicc)
-  aicc$deltaAICc <- aicc$AICc - min(aicc$AICc) 
-  aicc$L <- exp(-0.5*(aicc$deltaAICc))
-  aicc$weight <- aicc$L/(sum(aicc$L))
-  aicc$weight.pct <- aicc$weight*100
-  aicc <- arrange(aicc, aicc$deltaAICc)
-  return(aicc)
-}
 
