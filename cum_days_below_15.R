41bfd743504330b137766e944f36ec18d6116fef
anniekellner <annie.kellner@colostate.edu>
2021-01-17 21:21:28 -0700

    added df for histogram

--- cum_days_below_15.R
+++ cum_days_below_15.R
@@ -12,47 +12,60 @@
 
 rm(list = ls())
 
-load('all_v2.RData')
+#load('all_v2.RData')
+cox <- readRDS('./data/RData/cox_tdc.Rds')
+
 library(dplyr)
 library(data.table)
 library(ggplot2)
-
-SIC <- subset(all.v2, SIC >=0, na.rm=TRUE)
-SIC$SIC <- SIC$SIC*2
-SIC <- SIC %>%
-  filter(!(id=='pb_20333.2008' | id=='pb_20413.2006' | id=='pb_20418.2005' | id=='pb_20520.2012' | id=='pb_20529.2004')) #remove undecided
+library(stringr)
+library(tidyr)
 
 # Flag columns with <15% SIC
 
-flag.15 = SIC %>%
+flag = cox %>%
   group_by(id) %>%
-  arrange(id, datetime) %>%
-  mutate(flag.15 = ifelse(SIC<15,1,0))
+  arrange(id, tstart) %>%
+  mutate(flag = ifelse(SIC<15,1,0))
+
+flag = flag %>%
+  group_by(id) %>%
+  mutate(days_under15 = cumsum(flag))
+
+sum <- flag %>%
+  group_by(id) %>%
+  summarise(days = sum(flag)) 
+
+sum %>% 
+  separate(id, c("pb", "animal", "year"))
 
 # add time
 
-x= flag.15 %>%
-  group_by(id) %>%
-  arrange(id,datetime) %>%
-  mutate(time.15 = ifelse(flag.15==0 | lag(flag.15)==0,
-         0, difftime(datetime, lag(datetime), units='days'))) %>%
-  mutate(cumtime.15 = cumsum(ifelse(is.na(time.15), 0, time.15)) + time.15*0) %>%
-  mutate(pct.days.below15 = cumtime.15/30) %>%
-  mutate(index=difftime(last(ymd), ymd, units='days')) # so day 1 is day 1 for all bears, regardless of month/year
+#x= flag.15 %>%
+  #group_by(id) %>%
+  #arrange(id,datetime) %>%
+  #mutate(time.15 = ifelse(flag.15==0 | lag(flag.15)==0,
+         #0, difftime(datetime, lag(datetime), units='days'))) %>%
+  #mutate(cumtime.15 = cumsum(ifelse(is.na(time.15), 0, time.15)) + time.15*0) %>%
+  #mutate(pct.days.below15 = cumtime.15/30) %>%
+ # mutate(index=difftime(last(ymd), ymd, units='days')) # so day 1 is day 1 for all bears, regardless of month/year
 
-ice.calc <- as.data.frame(x)
-save(ice.calc, file='ice_calc.RData')
+#ice.calc <- as.data.frame(x)
+#save(ice.calc, file='ice_calc.RData')
 #-------------- GGPLOT ---------------------------------#
 
 # Number of days Spent in 15% SIC or less - all bears
 
-ggplot(ice.calc, aes(index, cumtime.15, color=id, na.rm=TRUE)) +
-  stat_summary(geom = 'line', fun.y = 'mean') +
-  scale_x_reverse() +
+ggplot(flag, aes(tstart, days_under15, color=id, na.rm=TRUE)) + # needs color line
+  geom_line() + 
   xlab("Days before Departure") +
   ylab('Cumulative # of days spent at < 15% SIC') +
   ggtitle('Days before departure spent at < 15% SIC') +
   theme_bw()
+
+# Histogram
+
+ggplot(data = )
 
 #saved C:\Users\akell\Desktop\Spring 2019\Presentations\Alaska\days_15pct.pdf
 
