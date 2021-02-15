0bc436b2f825b16a6e953628695f162d29098f5b
anniekellner <annie.kellner@colostate.edu>
2021-02-15 13:11:26 -0700

    initial commit: compare cox to parametric.

--- parametric_model.R
+++ parametric_model.R
@@ -15,65 +15,10 @@ ph <- readRDS('./data/RData/ph.Rds')
 
 source('MyFunctions.R') # create AICc table
 
-# Check SIC and make sure the models aren't crazy different when using 3-day moving window
-# Exclude year because do not have big enough sample size to include year as a factor 
+global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3 + dist_land3 + dist_pack3 + sd7 + ResidMass + SICsq3*speed3, 
+                      dist = "exp", data = ph, na.action = "na.fail")
 
+t <- dredge(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc", m.lim = c(2,3))
 
-SIC <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3, data = ph, cluster = id)
-speed <- flexsurvreg(Surv(tstart, tstop, migrate) ~ speed3, data = ph, cluster = id)
-land <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_land3, data = ph, cluster = id)
-pack <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_pack3, data = ph, cluster = id)
-sd <- flexsurvreg(Surv(tstart, tstop, migrate) ~ sd7, data = ph, cluster = id)
-#year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ year, data = ph, dist = "exp")
-
-SIC_speed <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3, data = ph, cluster = id)
-SIC_distland <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + dist_land3, data = ph, cluster = id)
-SIC_distpack <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + dist_pack3, data = ph, cluster = id)
-#SIC_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + year, data = ph, dist = "exp")
-
-speed_land <- flexsurvreg(Surv(tstart, tstop, migrate) ~ speed3 + dist_land3, data = ph, cluster = id)
-speed_pack <- flexsurvreg(Surv(tstart, tstop, migrate) ~ speed3 + dist_pack3, data = ph, cluster = id)
-speed_sd <- flexsurvreg(Surv(tstart, tstop, migrate) ~ speed3 + sd7, data = ph, cluster = id)
-#speed_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ speed3 + year, data = ph, dist = "exp")
-
-land_pack <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_land3 + dist_pack3, data = ph, cluster = id)
-#land_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_land3 + year, data = ph, dist = "exp")
-land_sd <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_land3 + sd7, data = ph, cluster = id)
-#land_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_land3 + year, data = ph, dist = "exp")
-
-pack_sd <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_pack3 + sd7, data = ph, cluster = id)
-#pack_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dist_pack3 + year, data = ph, dist = "exp")
-
-#sd_year <- flexsurvreg(Surv(tstart, tstop, migrate) ~ sd7 + year, data = ph, dist = "exp")
-
-aicc <- AICc(SIC, SIC_distland, SIC_distpack, SIC_speed, speed, speed_land, speed_pack, speed_sd, land, land_pack, land_sd, pack, pack_sd, sd)
-
-create_AICc_table(aicc)
-
-# ----- COX PROPORTIONAL HAZARD ---------------------------------------------------------------------------------------- #
-
-SIC <- coxph(Surv(tstart, tstop, migrate) ~ SICsq3, data = ph, cluster = id)
-speed <- coxph(Surv(tstart, tstop, migrate) ~ speed3, data = ph, cluster = id)
-land <- coxph(Surv(tstart, tstop, migrate) ~ dist_land3, data = ph, cluster = id)
-pack <- coxph(Surv(tstart, tstop, migrate) ~ dist_pack3, data = ph, cluster = id)
-sd <- coxph(Surv(tstart, tstop, migrate) ~ sd7, data = ph, cluster = id)
-#year <- coxph(Surv(tstart, tstop, migrate) ~ year, data = ph, dist = "exp")
-
-SIC_speed <- coxph(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3, data = ph, cluster = id)
-SIC_distland <- coxph(Surv(tstart, tstop, migrate) ~ SICsq3 + dist_land3, data = ph, cluster = id)
-SIC_distpack <- coxph(Surv(tstart, tstop, migrate) ~ SICsq3 + dist_pack3, data = ph, cluster = id)
-#SIC_year <- coxph(Surv(tstart, tstop, migrate) ~ SICsq3 + year, data = ph, dist = "exp")
-
-speed_land <- coxph(Surv(tstart, tstop, migrate) ~ speed3 + dist_land3, data = ph, cluster = id)
-speed_pack <- coxph(Surv(tstart, tstop, migrate) ~ speed3 + dist_pack3, data = ph, cluster = id)
-speed_sd <- coxph(Surv(tstart, tstop, migrate) ~ speed3 + sd7, data = ph, cluster = id)
-#speed_year <- coxph(Surv(tstart, tstop, migrate) ~ speed3 + year, data = ph, dist = "exp")
-
-land_pack <- coxph(Surv(tstart, tstop, migrate) ~ dist_land3 + dist_pack3, data = ph, cluster = id)
-#land_year <- coxph(Surv(tstart, tstop, migrate) ~ dist_land3 + year, data = ph, dist = "exp")
-land_sd <- coxph(Surv(tstart, tstop, migrate) ~ dist_land3 + sd7, data = ph, cluster = id)
-#land_year <- coxph(Surv(tstart, tstop, migrate) ~ dist_land3 + year, data = ph, dist = "exp")
-
-pack_sd <- coxph(Surv(tstart, tstop, migrate) ~ dist_pack3 + sd7, data = ph, cluster = id)
-
-speed_land
+interact <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3*speed3, 
+                            dist = "exp", data = ph, na.action = "na.fail")last.wa
