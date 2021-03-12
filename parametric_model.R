c87ce981a31263c457c1c3faac9e801861840055
anniekellner <annie.kellner@colostate.edu>
2021-03-12 12:47:16 -0700

    include wind direction in variable list

--- parametric_model.R
+++ parametric_model.R
@@ -10,23 +10,25 @@ library(tidyr)
 library(dplyr)
 library(tidyverse)
 
-ph <- readRDS('./data/RData/ph.Rds')
+ph <- readRDS('./data/RData/ph_Mar12.Rds')
 
 #source('MyFunctions.R') # create AICc table
 
-global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3 + dist_land3 + dist_pack3 + sd7 + ResidMass + SICsq3*speed3, 
-                      dist = "exp", data = ph, na.action = "na.fail")
+global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3 + dist_land3 + dist_pack3 + sd7 + ResidMass + SICsq3*speed3 + dir, 
+                      dist = "exp", method = "Nelder-Mead", data = ph, na.action = "na.fail")
 
 t <- dredge(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc", m.lim = c(1,3))
 
 tt <- t[1:10,]
+
+fit_dir <- flexsurvreg(Surv(tstart, tstop, migrate) ~ dir, dist = "exp", data = ph, na.action = "na.fail")
 
 fit <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SICsq3 + speed3 + dist_pack3, 
                    data = ph, dist = "exp", method = "Nelder-Mead", na.action = "na.fail")
 
 write.csv(tt, file = './data/derived-data/top_models.csv')
 
-top <- get.models(t, subset = 1)
+top <- get.models(t, subset = 1:10)
 
 # ------    CREATE NEW DATA TO PREDICT OVER ---------------- #
 
