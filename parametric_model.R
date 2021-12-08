1428d34823803d151b0eb4cd58ca5947e8197b89
anniekellner <annie.kellner@colostate.edu>
2021-12-08 13:38:36 -0700

    add function to show coefficient values in MuMIn for flexsurvreg objects

--- parametric_model.R
+++ parametric_model.R
@@ -13,52 +13,26 @@ library(tidyverse)
 ph <- readRDS('./data/RData/ph_Dec7.Rds')
 
 source("fxn_tidy_flexsurv.R")
+source("flexsurvreg_fxns.R") # to make coefficients show up when using flexsurvreg with MuMIn
 
 #source('MyFunctions.R') # create AICc table
 
-global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SIC_mean + speed3_max_mean + dist_land + dist_pack + sd7 + ResidMass + SIC_mean*speed3_max_mean + dir, 
-                      dist = "exp", method = "Nelder-Mead", data = ph, na.action = "na.fail")
+global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SIC_mean + speed3_max_mean + dist_land + dist_pack + sd7 + ResidMass + dir, 
+                      dist = "exp", method = "Nelder-Mead", data = ph, cl = 0.95, na.action = "na.fail")
 
 t <- dredge(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc", m.lim = c(1,3))
 
+
+#sw(subset(t, delta <= 4)) # Variable weights
+
+#model.avg(t, subset = delta <=4) 3 #model-averaged coefficients
+
 tt <- t[1:10,]
-
-#test <- get.models(t, subset = delta < 4)
-
 
 write.csv(tt, file = './data/derived-data/top_models.csv')
 
-top <- get.models(t, subset = 1:10)
+top <- get.models(t, subset = 2) # most parsimonious model (#2 in AICc ranking)
+tidytop <- tidy.flexsurvreg(top)
 
-top
-# ------    CREATE NEW DATA TO PREDICT OVER ---------------- #
+write.csv(top, file = './data/derived-data/best_model.csv')
 
-source("fxn_predict_flexsurv.R") # run script for predict function on flexsurv object
-
-new_15 <- data.frame(speed3 = seq(0,20, length.out = 100), SICsq3 = 10000, dist_pack3 = 200, tstart = seq(1,100,1), tstop = seq(2, 101, 1)) # SIC at 15%
-
-p <- predict.flexsurvreg(fit, newdata = new_15, type = "survival", na.action = "na.pass", times = 1)
-p$index <- seq(0,20,length.out = 100)
-
-qplot(data = p, x = index, y = .pred, geom = "line")
-
-head(p)
-
-colnames(p) <- c("pred_15", "windspeed")
-
-new_30 <- data.frame(speed3 = seq(0,11, length.out = 100), SICsq3 = 900, dist_pack3 = 38.06, tstart = seq(1,100,1), tstop = seq(2, 101, 1))
-p30 <- predict.flexsurvreg(fit, newdata = new_30, type = "response", na.action = "na.pass", times = 1)
-head(p30)
-
-p$pred_30 <- p30
-
-colnames(p) <- c("pred_15", "ws", "pred_30")
-
-ggplot(data = p, aes(x = ws, y = pred_15)) + 
-  geom_line(color = "black") + 
-  geom_line(aes(y = pred_30$.pred), color = "red")
-  
-
-
-
-         
