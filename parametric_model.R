c5ee974028f6dcaae7deb31e70bcc31474a0877e
anniekellner <annie.kellner@colostate.edu>
2022-04-25 16:27:17 -0600

    subset models so that no models are included that have only one wind variable

--- parametric_model.R
+++ parametric_model.R
@@ -11,19 +11,17 @@ library(dplyr)
 library(tidyverse)
 library(survival)
 
-ph <- readRDS('./data/RData/ph_Dec7.Rds')
+ph <- readRDS('./data/RData/ph_Apr25_2022.Rds')
 
 source("fxn_tidy_flexsurv.R")
 source("flexsurvreg_fxns.R") # to make coefficients show up when using flexsurvreg with MuMIn
 
 #source('MyFunctions.R') # create AICc table
 
-global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SIC_mean + speed3_max_mean + dist_land + dist_pack + sd7 + ResidMass + dir, 
+global.model <- flexsurvreg(Surv(tstart, tstop, migrate) ~ SIC_mean + speed3_max + dist_land + dist_pack + sd7 + ResidMass + wind_cos + wind_sin, 
                       dist = "exp", method = "Nelder-Mead", data = ph, cl = 0.95, na.action = "na.fail")
 
-t <- dredge(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc", m.lim = c(1,3))
-
-sw(t) # sum of weights
+t <- dredge(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc", m.lim = c(1,4), subset = (wind_sin | !wind_cos) && (wind_cos | ! wind_sin))
 
 t[1]
 
