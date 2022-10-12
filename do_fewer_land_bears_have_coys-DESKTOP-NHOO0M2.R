fd25411c1c574babd05af1a25e3848db9b7371e4
anniekellner <annie.kellner@colostate.edu>
2022-10-12 14:41:19 -0600

    initial commit

---
+++ do_fewer_land_bears_have_coys-DESKTOP-NHOO0M2.R
@@ -0,0 +1,28 @@
+##################################################
+##    DO FEWER LAND BEARS HAVE COYS?  ############
+##################################################
+
+## There is no association between ice bears and coys (land bears and ice bears have coys in ~equal measure)
+
+library(dplyr)
+
+rm(list = ls())
+
+# ------  Load data --------------- #
+
+repro <- readRDS('./data/derived-data/repro.Rds')
+
+chi <- select(repro, repro, bear_type)
+
+table(chi$bear_type, chi$repro)
+
+# Run chi-squared test with three categories (den, coy, yearling)
+
+chisq.test(chi$bear_type, chi$repro) # not significant
+
+# Run with two categories (coy, not coy)
+
+chi$coy <- ifelse(chi$repro == "coy", "coy", "not coy")
+head(chi)
+
+chisq.test(repro$bear_type, repro$coy) # not significant
